48f75eb4dfb863127711f450e1549ddae92c800e
German Renato Cobian <germancobian@hotmail.com>
2021-03-15 12:38:12 -0700

    Create trailing_space? error check

--- lib/check_error.rb
+++ lib/check_error.rb
@@ -7,3 +7,17 @@ class ErrorCheck
     @lint_action = FileReader.new(file_path)
     @error_messages = []
   end
+
+  def check_errors
+    trailing_space?
+  end
+
+  def trailing_space?
+  @lint_action.line_by_line.each_with_index do |line, i|
+      if line[-2] == ' ' && !line.strip.empty?
+        @error_messages << { line_num: i + 1, message: 'Trailing whitespace detected' } 
+      end
+    end
+  end
+
+end
