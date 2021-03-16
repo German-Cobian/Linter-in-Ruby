e595ddde4b1d8a617b6502582af674409cd4d7ab
German Renato Cobian <germancobian@hotmail.com>
2021-03-16 07:47:31 -0700

    resolve linter issues

--- bin/main.rb
+++ bin/main.rb
@@ -1,20 +1,24 @@
-require_relative '../lib/file_reader.rb'
-require_relative '../lib/check_error.rb'
+require_relative '../lib/file_reader'
+require_relative '../lib/check_error'
 
 def show_errors(check)
   system('cls') || system('clear')
-  check.error_messages.each { |error| puts "Line #{error[:line_num]}: #{error[:message]}" }
+  check.error_messages.each { |error| puts "\nLine #{error[:line_num]}: #{error[:message]}" }
 end
-
+puts
 puts 'Please type path of file you intend to check:'
 file_path = gets.chomp
 check = ErrorCheck.new(file_path)
 go_ahead = FileReader.new(file_path)
 if go_ahead.valid
+  puts
   puts "Checking #{file_path} for errors."
   check.check_errors
   show_errors(check)
+  puts
   puts "#{check.error_messages.size} offenses detected"
+  puts
 else
+  puts
   puts "Could not open file #{file_path}"
 end
